4c1f2840f7cefb14a476d269dbf945cc98f0d10f
Nazarett <gnazarett@gmail.com>
2015-06-16 21:04:43 -0400

    add file

---
+++ Prompt & Alert/script.js
@@ -0,0 +1,8 @@
+//Prompt and Alert
+
+var width = prompt("We are calcualting the area of a rectangle.\nPlease enter the width");
+var height = prompt("Pleas enter the height");
+var area = width * height;
+console.log(area);
+var result = "The area of your rectangle is" + area + " sq feet";
+alert(result);
