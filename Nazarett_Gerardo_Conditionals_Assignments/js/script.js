b2b050c43aed4112070593264043e3d392ede284
Nazarett <gnazarett@gmail.com>
2015-06-18 20:51:28 -0400

    Adding info

--- Nazarett_Gerardo_Conditionals_Assignments/js/script.js
+++ Nazarett_Gerardo_Conditionals_Assignments/js/script.js
@@ -1,3 +1,14 @@
 //Gerardo D. Nazarett
 //June 06 2015
 //Conditionals Assignments
+
+alert("We are calculating the height of your family");
+var momHeight = prompt("What is your mother's height?");
+    console.log(momHeight);
+var dadHeight = prompt("What is you father's height");
+    console.log(dadHeight);
+var myHeight = prompt("What is your height");
+    console.log(myHeight)
+if(momHeight > 0){
+
+}
