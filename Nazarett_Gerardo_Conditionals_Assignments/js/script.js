0d43ed24db78178f8a762e75c658787117b90d4f
Nazarett <gnazarett@gmail.com>
2015-06-18 21:06:36 -0400

    adding comments and mom info

--- Nazarett_Gerardo_Conditionals_Assignments/js/script.js
+++ Nazarett_Gerardo_Conditionals_Assignments/js/script.js
@@ -2,13 +2,21 @@
 //June 06 2015
 //Conditionals Assignments
 
-alert("We are calculating the height of your family");
-var momHeight = prompt("What is your mother's height?");
+alert("We are calculating the height of your family"); //Alert to inform user what are we doing
+var momHeight = prompt("What is your mother's height?"); //Prompt for mother's height
     console.log(momHeight);
+        (momHeight > 0) ? alert("Thank you"):alert("Invalid entry"); //Example of ternary operator
+if(momHeight > 0){ //If  mother's height larger than zero continue
+    console.log(momHeight)
+
+}else{ alert("Restart")
+
+}
+
 var dadHeight = prompt("What is you father's height");
     console.log(dadHeight);
 var myHeight = prompt("What is your height");
     console.log(myHeight)
-if(momHeight > 0){
 
-}
+
+
