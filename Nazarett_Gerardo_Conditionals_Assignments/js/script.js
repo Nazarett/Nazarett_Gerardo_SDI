eb293cc56cf0f225a843fd73a3e83978542c6dcf
Nazarett <gnazarett@gmail.com>
2015-06-18 21:57:15 -0400

    Entering more comments

--- Nazarett_Gerardo_Conditionals_Assignments/js/script.js
+++ Nazarett_Gerardo_Conditionals_Assignments/js/script.js
@@ -8,28 +8,28 @@ var momHeight = prompt("What is your mother's height in ft?"); //Prompt for moth
         (momHeight > 0) ? alert("Thank you"):alert("Invalid entry"); //Example of ternary operator
 if(momHeight > 0){ //If  mother's height larger than zero continue
     console.log(momHeight)
-    var dadHeight = prompt("What is you father's height in ft");
+    var dadHeight = prompt("What is you father's height in ft");//Prompt father's height
     console.log(dadHeight);
-    if(dadHeight > 0){alert("Thank you")
+    if(dadHeight > 0){alert("Thank you") //Thank for proper entry
         console.log(dadHeight)
-        var myHeight = prompt("What is your height");
+        var myHeight = prompt("What is your height");//Prompt your height
         console.log(myHeight)
-        if(myHeight > 0){alert("Thank you")
+        if(myHeight > 0){alert("Thank you") //Thanks for proper entry
             console.log(myHeight)
-        }else{alert("Invalid entry \nPlease restart")
+        }else{alert("Invalid entry \nPlease restart") //Invalid entry
 
         }
 
 
-    }else{alert("Invalid entry \nPlease restart")
+    }else{alert("Invalid entry \nPlease restart") //Invalid entry
 
     }
-}else{ alert("Please restart")
+}else{ alert("Please restart") //Invalid entry
 
 }
-averageHeight = (momHeight) + (dadHeight) + (myHeight);
+var averageHeight = momHeight += dadHeight += myHeight; //Trying to calculate average
 console.log(averageHeight)
-var result = ("Your family average height is " + averageHeight + "ft")
+var result = ("Your family average height is " + averageHeight + "ft") //Final result
 alert(result)
 
 
