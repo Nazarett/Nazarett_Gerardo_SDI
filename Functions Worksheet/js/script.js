0f8b5f57360d30840a4e2315d5ec96723dce27ba
Nazarett <gnazarett@gmail.com>
2015-06-22 22:39:16 -0400

    Console log and message comment

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -9,7 +9,7 @@ var calcCircumference = function(Pi, radius){ //Function for circumference
 
 var c = calcCircumference(3.14, 5); //Adding variable c with values
 
-console.log("The circumference of the circle is " + c);
+console.log("The circumference of the circle is " + c); //Console log plus message
 
 //Stung!
 
@@ -22,4 +22,4 @@ var calcCamelStings = function(camelWeight, stingsPerLbs){ //Function for camel 
 }
 
 var s = calcCamelStings(720, 8.666666667) //Adding variable s with values
-console.log("It takes " + c + "bee stings to kill this animal. ")
+console.log("It takes " + c + "bee stings to kill this animal. ") //Console log plus message
