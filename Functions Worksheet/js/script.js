8e7ae8d495aa777dc5fa32812ede3d248899b583
Nazarett <gnazarett@gmail.com>
2015-06-22 21:49:32 -0400

    Adding variable c

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -1,7 +1,10 @@
 //Circumference
 //Calculate the circumference of a circle
 
-var calcCircumference = function(Pi, raidus){
+var calcCircumference = function(Pi, radius){
     var circumference = 2 * Pi * radius
     return circumference;
 }
+
+var c = calcCircumference(3.14, 5)
+
