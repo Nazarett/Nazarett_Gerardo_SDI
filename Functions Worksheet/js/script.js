afee82db197cacdde734c67bc6a04d4b8ac40cbd
Nazarett <gnazarett@gmail.com>
2015-06-22 22:22:59 -0400

    Adding semicolon

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -1,21 +1,21 @@
 //Circumference
 //Calculate the circumference of a circle
-var radius = 5 //Variable radius and value
-var Pi = 3.14 //Variable Pi and value
+var radius = 5; //Variable radius and value
+var Pi = 3.14; //Variable Pi and value
 var calcCircumference = function(Pi, radius){ //Function for circumference
-    var circumference = 2 * Pi * radius //Adding circumference formula
+    var circumference = 2 * Pi * radius; //Adding circumference formula
     return circumference; //Return value
 }
 
-var c = calcCircumference(3.14, 5) //Adding variable c with values
+var c = calcCircumference(3.14, 5); //Adding variable c with values
 
-console.log("The circumference of the circle is " + c)
+console.log("The circumference of the circle is " + c);
 
 //Stung!
 
-var camelWeight = 720 //Camel weight
-var stingsPerLbs = 8.666666667 // Stings per pound
+var camelWeight = 720; //Camel weight
+var stingsPerLbs = 8.666666667; // Stings per pound
 var calcCamelStings = function(camelWeight, stingsPerLbs){ //Function for camel stings
-    var camelStings = camelWeight * stingsPerLbs //Camel sting formula
+    var camelStings = camelWeight * stingsPerLbs; //Camel sting formula
 
 }
