c499044b5611dab5d5b0f49398fbfd8c3f392a53
Nazarett <gnazarett@gmail.com>
2015-06-22 21:54:37 -0400

    Comment

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -3,7 +3,7 @@
 
 var calcCircumference = function(Pi, radius){ //Adding function
     var circumference = 2 * Pi * radius //Adding circumference formula
-    return circumference;
+    return circumference; //Return value
 }
 
 var c = calcCircumference(3.14, 5) //Adding variable c with values
