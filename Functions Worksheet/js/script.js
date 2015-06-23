30f55f7339c7155f3326ba1a9fb193a35c07d28a
Nazarett <gnazarett@gmail.com>
2015-06-22 22:35:54 -0400

    Console log

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -22,3 +22,4 @@ var calcCamelStings = function(camelWeight, stingsPerLbs){ //Function for camel 
 }
 
 var s = calcCamelStings(720, 8.666666667) //Adding variable s with values
+console.log("It takes " + c + "bee stings to kill this animal. ")
