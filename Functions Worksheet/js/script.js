fbf7c9f2becc54e969067e556d43a5cb8181b246
Nazarett <gnazarett@gmail.com>
2015-06-22 21:59:07 -0400

    Pi variable and value

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -1,7 +1,7 @@
 //Circumference
 //Calculate the circumference of a circle
 var radius = 5 //Circumference radius
-var Pi = 3.14
+var Pi = 3.14 //Variable Pi and value
 var calcCircumference = function(Pi, radius){ //Adding function
     var circumference = 2 * Pi * radius //Adding circumference formula
     return circumference; //Return value
