e4005efaba57557241dd715ad6f80d28c283631a
Nazarett <gnazarett@gmail.com>
2015-06-22 22:39:48 -0400

    Semicolon forgoten

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -21,5 +21,5 @@ var calcCamelStings = function(camelWeight, stingsPerLbs){ //Function for camel 
 
 }
 
-var s = calcCamelStings(720, 8.666666667) //Adding variable s with values
-console.log("It takes " + c + "bee stings to kill this animal. ") //Console log plus message
+var s = calcCamelStings(720, 8.666666667); //Adding variable s with values
+console.log("It takes " + c + "bee stings to kill this animal. "); //Console log plus message
