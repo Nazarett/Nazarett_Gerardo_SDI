0d24f926d7937a71a699704d4b379044589fa53e
Nazarett <gnazarett@gmail.com>
2015-06-22 22:19:03 -0400

    Function comment

--- Functions Worksheet/js/script.js
+++ Functions Worksheet/js/script.js
@@ -2,7 +2,7 @@
 //Calculate the circumference of a circle
 var radius = 5 //Variable radius and value
 var Pi = 3.14 //Variable Pi and value
-var calcCircumference = function(Pi, radius){ //Adding function
+var calcCircumference = function(Pi, radius){ //Function for circumference
     var circumference = 2 * Pi * radius //Adding circumference formula
     return circumference; //Return value
 }
@@ -14,4 +14,7 @@ console.log("The circumference of the circle is " + c)
 //Stung!
 
 var camelWeight = 720 //Camel weight
-var stingsPerLbs = 8.666666667
+var stingsPerLbs = 8.666666667 // Stings per pound
+var calcCamelStings = function(camelWeight, stingsPerLbs){ //Function for camel stings
+
+}
