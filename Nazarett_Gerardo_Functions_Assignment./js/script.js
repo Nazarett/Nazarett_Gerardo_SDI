f4bd41d3245df947dafed6ece8166574febb781f
Nazarett <gnazarett@gmail.com>
2015-06-24 22:52:17 -0400

    code

--- Nazarett_Gerardo_Functions_Assignment./js/script.js
+++ Nazarett_Gerardo_Functions_Assignment./js/script.js
@@ -6,12 +6,13 @@ Functions Assignment
 */
 
 //Variables
-var myNum = prompt("Please choose your first \n number between 1 and 53") //Variable with prompt to choose a number
+var myNum = prompt("Please choose your first \n number between 1 and 53"); //Variable with prompt to choose a number
 
 //Functions
 function numApproval(numSelected){
-
+    
 }
 
 //Code
 
+numApproval(myNum);
