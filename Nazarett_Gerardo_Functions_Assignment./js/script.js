d6f4bb052d06103dc8c4039bdddeaa52196b2b9a
Nazarett <gnazarett@gmail.com>
2015-06-25 22:42:03 -0400

    testing function and random numbers

--- Nazarett_Gerardo_Functions_Assignment./js/script.js
+++ Nazarett_Gerardo_Functions_Assignment./js/script.js
@@ -6,36 +6,61 @@ Functions Assignment
 */
 
 //Variables
-alert ("Please choose between Powerball or Florida Lottery");
+//alert ("Welcome, ready to play Powerball or Florida Lottery"); //Alert to welcome user and giving him information for whats next
 
-var tickets = prompt ("Would you like to play \nPowerball or Florida lottery");
+//var tickets = prompt ("Would you like to play \nPowerball or Florida lottery \nPlease type your answer"); //variable with prompt
 
+var myLotNum;
 
 //Functions
 
-function selectionValidationOne(lotteryOne) {
+function selectionValidationOne(lotteryOne) { //Starting function
 
- var clicks = 1;
+ var clicks = 1; //variable for clicks
 
- while (lotteryOne === "") {
+ while (lotteryOne === "") { //Loop and condition
 
-  lotteryOne = prompt("Invalid entry.");
+  lotteryOne = prompt("Invalid entry."); //Prompt message when leaving blank space
 
   clicks++;
 
-  if(clicks === 3){
+  if(clicks === 3){ // clicks conditions
 
-   console.log("Enough times leaving blanks.")
-   break;
+   console.log("Enough times leaving blanks.") //message for limit of clicking
+   break; // break will stop infinite loop
 
   }
 
  }
 
-return lotteryOne;
+return lotteryOne; //returning values of lotteryOne back to code
+
+}
+
+//Lottery function assuming user choose Florida Lottery
+
+function lotteryNum (min, max){
+
+
+ var randomNum = Math.random() * (max -min) + min;
+
+randomNum = Math.round(randomNum);
+
+ return randomNum;
+
 }
 
 
 //Code
-tickets = selectionValidationOne(tickets);
-console.log("Congratulations you choose " + tickets + " .")
+//tickets = selectionValidationOne(tickets);
+//console.log("Congratulations you choose " + tickets + " .") //Message leting user kow his selection
+ myLotNum = lotteryNum(1, 13);
+console.log(myLotNum);
+myLotNum = lotteryNum(14, 23);
+console.log(myLotNum);
+myLotNum = lotteryNum(24, 33);
+console.log(myLotNum);
+myLotNum = lotteryNum(36, 43);
+console.log(myLotNum);
+myLotNum = lotteryNum(44, 53);
+console.log(myLotNum);
